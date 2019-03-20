707a4ebbc8ba3bc9e3d523e0f493f4c57aa87a13
AnuBudhavarapu <anu.budhavarapu@gmail.com>
2019-03-20 14:23:05 +0530

    Methods for Shuffling ,Dealing one card at a time ,Reset added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { Deck } from "../app/deck/deck"
+declare let require: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Dc-take-home';
+  public title = 'Deck-of-cards';
+  public totalCardsLeft: number = 51;
+  public imageArray = new Array();
+  constructor() {
+  }
+  public cardsLeft: number = this.totalCardsLeft;
+  public isEdited: boolean = false;
+  public isDisabled: boolean = false;
+  public modifiedImageArray = new Array();
+  public deck = new Deck();
+  // gets all the cards from the deck and returns images
+  getAllCards() {
+    this.reset();
+    let imageArray = this.deck.getAllCardsFortheDeck();
+    return imageArray;
+  }
+  //gets the shuffled cards from deck and displays it 
+  getshuffledCards() {
+    this.isEdited = false;
+    let gallery = document.getElementById("cardGallery");
+    let imgArray = this.deck.getShuffledCardDeck(this.imageArray);
+    for (let i = 0; i < imgArray.length; i++) {
+      gallery.appendChild(imgArray[i]);
+    }
+    this.cardsLeft = -1;
+    this.isDisabled = true;
+  }
+  /*gives out one card for every click and count is displayed
+  after all the cards are dealt and value is zero and if still the 
+  user click on it the deal  button is disabled*/
+  dealOne() {
+    this.isEdited = true;
+    let imgArray = this.modifiedImageArray;
+    if (this.cardsLeft == this.totalCardsLeft) {
+      imgArray = this.deck.getShuffledCardDeck(this.imageArray);
+      this.modifiedImageArray = imgArray;
+    }
+    let gallery = document.getElementById("cardGallery");
+    if (this.cardsLeft != -1) {
+      gallery.appendChild(imgArray[this.cardsLeft]);
+      this.cardsLeft -= 1;
+    }
+    else {
+      this.isEdited = false;
+      this.isDisabled = true;
+    }
+  }
+  //removes all the cards from the deck
+  reset() {
+    this.cardsLeft = this.totalCardsLeft;
+    let gallery = document.getElementById("cardGallery");
+    while (gallery.firstChild) {
+      gallery.removeChild(gallery.firstChild);
+      this.isDisabled = false;
+      this.isEdited=false;
+    }
+  }
+  ngOnInit() {
+    this.isDisabled = false;
+    this.imageArray = this.getAllCards();
+    let shuffleBtn = document.getElementById("shuffle") as HTMLButtonElement;
+    shuffleBtn.addEventListener("click", (e: Event) => this.getshuffledCards());
+    let resetBtn = document.getElementById("reset") as HTMLButtonElement;
+    resetBtn.addEventListener("click", (e: Event) => this.reset());
+    let dealBtn = document.getElementById("dealOne") as HTMLButtonElement;
+    dealBtn.addEventListener("click", (e: Event) => this.dealOne());
+  }
 }
--- src/app/deck/deck.ts
+++ src/app/deck/deck.ts
@@ -1,2 +1,44 @@
+
+import { Card } from "../card/card"
+import { CardType } from '../cardType/card-type';
+import { Rank } from "../rank/rank"
 export class Deck {
+    cardTypes: CardType[]= [CardType.SPADE, CardType.CLUB, CardType.DIAMOND, CardType.HEART];
+    ranks: Rank[] = [Rank.ACE, Rank.TWO, Rank.THREE, Rank.FOUR, Rank.FIVE, Rank.SIX, Rank.SEVEN, Rank.EIGHT, Rank.NINE, Rank.TEN, Rank.JACK, Rank.QUEEN, Rank.KING];
+    card: { rank: Rank, CardType: CardType };
+
+    constructor() {
+    }
+    //Add all the cards to the deck for display
+    getAllCardsFortheDeck() {
+        let imgArray = new Array();
+        let count = 0;
+        let deck = [];
+        this.cardTypes.forEach((type) =>
+            this.ranks.forEach((rank) => {
+                let playCard = new Card(rank, type, "");
+                playCard.imageName = require('../../assets/' + playCard.imageName());
+                imgArray[count] = new Image();
+                imgArray[count].src = playCard.imageName;
+                deck.push(playCard)
+                count = count + 1;
+            }));
+
+        return imgArray;
+    }
+   //Shuffles the cards based on randomly generated number
+    getShuffledCardDeck(imageArray) {
+        let imgArray = new Array();
+        imgArray = imageArray;
+        let currentVal = imgArray.length;
+        while (currentVal !== 0) {
+            let randomNum = Math.floor(Math.random() * Math.floor(currentVal));
+            currentVal -= 1;
+            let interChange = imgArray[currentVal];
+            imgArray[currentVal] = imgArray[randomNum];
+            imgArray[randomNum] = interChange;
+        }
+        return imgArray;
+    }
+
 }
